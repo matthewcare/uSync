7e9080c5e60fa5771bee314f39c229dcc6c03819
Kevin Jump <kevin@thejumps.co.uk>
2024-10-29 12:00:37 +0000

    documentation

--- uSync.Core/ISyncItemFactory.cs
+++ uSync.Core/ISyncItemFactory.cs
@@ -13,34 +13,59 @@ namespace uSync.Core;
 /// </summary>
 public interface ISyncItemFactory
 {
-    // serializers
+    /// <summary>
+    ///  get all the serializers for a type of object
+    /// </summary>
     IEnumerable<ISyncSerializer<TObject>> GetSerializers<TObject>();
 
+    /// <summary>
+    ///  get a serializer by name
+    /// </summary>
     ISyncSerializer<TObject>? GetSerializer<TObject>(string name);
 
-    // tracking items
+    /// <summary>
+    ///  get all the Sync trackers for a type of object
+    /// </summary>
     IEnumerable<ISyncTracker<TObject>> GetTrackers<TObject>();
+
+    /// <summary>
+    ///  get all the changes for a node compared to the current item in umbraco. 
+    /// </summary>
+    Task<IEnumerable<uSyncChange>> GetChangesAsync<TObject>(XElement node, SyncSerializerOptions options);
+
+    /// <summary>
+    ///  get all the changes between two xml elements, based on the item type
+    /// </summary>
+    Task<IEnumerable<uSyncChange>> GetChangesAsync<TObject>(XElement node, XElement currentNode, SyncSerializerOptions options);
+
+
+    /// <summary>
+    ///  get all the dependency checkers for a type of object
+    /// </summary>
+    /// <typeparam name="TObject"></typeparam>
+    /// <returns></returns>
+    IEnumerable<ISyncDependencyChecker<TObject>> GetCheckers<TObject>();
+
+    /// <summary>
+    ///  get all the possible dependencies for an object (based on the passed flags)
+    /// </summary>
+    Task<IEnumerable<uSyncDependency>> GetDependenciesAsync<TObject>(TObject item, DependencyFlags flags);
+
+    /// <summary>
+    ///  an entity cache - can be used to improve lookup times on large syncs. 
+    /// </summary>
+    SyncEntityCache EntityCache { get; }
 
     [Obsolete("use GetChangesAsync will be removed in v16")]
     IEnumerable<uSyncChange> GetChanges<TObject>(XElement node, SyncSerializerOptions options)
-        => GetChangesAsync<TObject>(node, options).Result;
-    Task<IEnumerable<uSyncChange>> GetChangesAsync<TObject>(XElement node, SyncSerializerOptions options);
+    => GetChangesAsync<TObject>(node, options).Result;
 
     [Obsolete("use GetChangesAsync will be removed in v16")]
     IEnumerable<uSyncChange> GetChanges<TObject>(XElement node, XElement currentNode, SyncSerializerOptions options)
         => GetChangesAsync<TObject>(node, currentNode, options).Result;
 
-    Task<IEnumerable<uSyncChange>> GetChangesAsync<TObject>(XElement node, XElement currentNode, SyncSerializerOptions options);
-
-    // dependency checker items
-    IEnumerable<ISyncDependencyChecker<TObject>> GetCheckers<TObject>();
-
     [Obsolete("Use GetDependenciesAsync will be removed in v16")]
     IEnumerable<uSyncDependency> GetDependencies<TObject>(TObject item, DependencyFlags flags)
         => GetDependenciesAsync(item, flags).Result;
-
-    Task<IEnumerable<uSyncDependency>> GetDependenciesAsync<TObject>(TObject item, DependencyFlags flags);
-
-    SyncEntityCache EntityCache { get; }
 
 }
